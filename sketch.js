2da85d9711710bbaf50a6b3d0e0a6c4a9b412b84
smitasg <63237649+smitasg@users.noreply.github.com>
2022-02-17 17:05:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+
+var box;
+function setup() {
+  createCanvas(400,400);
+  box = createSprite(200,200,100,100);
+
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyIsDown(RIGHT_ARROW))
+  {
+    box.x = box.x +2;
+  }
+  
+  if(keyIsDown(LEFT_ARROW))
+  {
+    box.x = box.x -2;
+  }
+drawSprites();
+}
+
+
+
+
